e57b2ea1b672be06337a9a8610efffae37b4f713
András Kiss <akiss@gamma.ttk.pte.hu>
2018-08-09 14:02:30 +0200

    last day

--- 180716/dec.py
+++ 180716/dec.py
@@ -1,13 +1,34 @@
 #!/usr/bin/enc python
 
-# Here is a first attempt at porting the deconvolution algorithm
-# from FORTRAN to python. The gaussian filter is not yet implemented
-# in the program. Right now I do it with the plotting software (gnuplot),
-# but it would be better if the python program did it. Also, I haven't
-# done the command line argument interpreter yet, so the file name must
-# be changed in the code every time. A GUI would be nice, and a live plot
-# of the convoluted and deconvoluted image. For that, the XYZ data needs
-# to be converted to a matrix.
+'''
+Deconvolution of distorted SECM images.
+Copyright (C) 2018 András Kiss
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
+ Here is a first attempt at porting the deconvolution algorithm
+ from FORTRAN to python. The gaussian filter is not yet implemented
+ in the program. Right now I do it with the plotting software (gnuplot),
+ but it would be better if the python program did it. Also, I haven't
+ done the command line argument interpreter yet, so the file name must
+ be changed in the code every time. A GUI would be nice, and a live plot
+ of the convoluted and deconvoluted image. For that, the XYZ data needs
+ to be converted to a matrix.
+
+'''
 
 import numpy as np
 import subprocess
